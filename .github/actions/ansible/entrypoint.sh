e61f23153cb865d4e59fc78621e25dc41470c111
RaduLupan <github@lupan.ca>
2021-06-06 09:50:08 -0400

    Typo plus added missing playbook name

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-ansible-galaxy install -r roles/requirments.yml
+ansible-galaxy install -r roles/requirements.yml
 
 echo $ANSIBLE_VAULT_PASSWORD >> .vault
 
-ansible-playbook -i hosts_aws_ec2.yml --vault-password-file .vault
+ansible-playbook -i hosts_aws_ec2.yml site.yml --vault-password-file .vault
 
 # Avoids locally storing on a mounted volume
 rm .vault
