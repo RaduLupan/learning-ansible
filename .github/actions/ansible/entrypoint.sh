3a8da603cd1c8066b9ba28d88eb56ab8f7ceda1b
RaduLupan <github@lupan.ca>
2021-06-08 07:55:39 -0400

    Added relative path

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-ansible-galaxy install -r roles/requirements.yml
+ansible-galaxy install -r /aws/roles/requirements.yml
 
 echo $ANSIBLE_VAULT_PASSWORD >> .vault
 
-ansible-playbook -i hosts_aws_ec2.yml site.yml --vault-password-file .vault
+ansible-playbook -i hosts_aws_ec2.yml /aws/site.yml --vault-password-file .vault
 
 # Avoids locally storing on a mounted volume
 rm .vault
