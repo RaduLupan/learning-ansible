ec46726889cb650946c019878acf072181db2865
RaduLupan <github@lupan.ca>
2021-06-11 08:08:19 -0400

    Commented out commands and inserted simple commends to determine content of the mounted volume

--- aws/entrypoint.sh
+++ aws/entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-ansible-galaxy install -r roles/requirements.yml
+#ansible-galaxy install -r roles/requirements.yml
 
-echo $ANSIBLE_VAULT_PASSWORD >> .vault
+#echo $ANSIBLE_VAULT_PASSWORD >> .vault
 
-ansible-playbook -i hosts_aws_ec2.yml site.yml --vault-password-file .vault
+#ansible-playbook -i hosts_aws_ec2.yml site.yml --vault-password-file .vault
 
 # Avoids locally storing on a mounted volume
-rm .vault
+#rm .vault
+
+pwd
+ls
