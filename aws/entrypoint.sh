44cc60ff8c60100ac0d3e947a82866e837dd041c
RaduLupan <github@lupan.ca>
2021-06-11 08:14:34 -0400

    Changed dir to aws

--- aws/entrypoint.sh
+++ aws/entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-#ansible-galaxy install -r roles/requirements.yml
+cd aws
+ansible-galaxy install -r roles/requirements.yml
 
-#echo $ANSIBLE_VAULT_PASSWORD >> .vault
+echo $ANSIBLE_VAULT_PASSWORD >> .vault
 
-#ansible-playbook -i hosts_aws_ec2.yml site.yml --vault-password-file .vault
+ansible-playbook -i hosts_aws_ec2.yml site.yml --vault-password-file .vault
 
 # Avoids locally storing on a mounted volume
-#rm .vault
-
-pwd
-ls
+rm .vault
