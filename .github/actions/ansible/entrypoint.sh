fd1806b3ad0e418b825095214154a1a366467d33
RaduLupan <github@lupan.ca>
2021-06-12 08:23:14 -0400

    Deleted unused files

--- .github/actions/ansible/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ansible-galaxy install -r roles/requirements.yml
-
-echo $ANSIBLE_VAULT_PASSWORD >> .vault
-
-ansible-playbook -i hosts_aws_ec2.yml site.yml --vault-password-file .vault
-
-# Avoids locally storing on a mounted volume
-rm .vault
